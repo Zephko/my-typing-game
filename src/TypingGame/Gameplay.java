bb4c24c0644b4b39e84badfd786e1095c63b460d
Zephaniah Ko <zephaniahko@gmail.com>
2019-08-15 16:31:59 -0700

    Green and white letters working, red not working but in progress

--- src/TypingGame/Gameplay.java
+++ src/TypingGame/Gameplay.java
@@ -12,7 +12,8 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
     private boolean play = false;
 
     private String textblock = "...he's the hero Gotham deserves, but not the one it needs right now. So, we'll hunt him, because he can take it. Because he's not our hero. He's a silent guardian. A watchful protector. A Dark Knight.";
-    private int wordstyped = 0;
+    private int lettersCorrect = 0;
+    private int lettersIncorrect = 0;
     private String lettersTyped = "";
 
     private Timer timer;
@@ -30,17 +31,19 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
     }
 
     public void paint(Graphics g) {
-    /* //full black background
+    //full black background
         //background
         g.setColor(Color.black);
         int width = Main.width;
         int height = Main.height;
         g.fillRect(1, 1, width, height);
-    */
+
         //text block
-        text.draw(g);
+        //TODO: add parameter that tracks how many words correctly typed so far
+        text.draw(g, lettersCorrect,lettersIncorrect);
 
         //letters being typed
+        g.setColor(Color.white);
         g.setFont(new Font("sans serif", Font.BOLD, 25));
         g.drawString(lettersTyped, 10, 500);
 
@@ -58,13 +61,54 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        System.out.println(e.getKeyChar());
-        lettersTyped += e.getKeyChar();
-
+        //TODO: create end sequence when all characters typed
 
         if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE && lettersTyped.length() != 0){
+        //2 branches here: one if correct letter backspaced, one if incorrect letter backspaced
+            if (lettersTyped.charAt(lettersTyped.length() - 1) == ' '){
+
+            } else if (lettersTyped.charAt(lettersTyped.length() - 1) == text.getChar(lettersCorrect - 1)) {
+                System.out.println("correct char backspaces");
+                lettersCorrect--;
+
+            } else {
+                lettersIncorrect--;
+            }
+
+            System.out.println("backspace hit");
             lettersTyped = lettersTyped.substring(0, lettersTyped.length() - 1);
+            System.out.println(lettersTyped);
         }
+
+        else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            System.out.println("spacebar hit");
+
+            //if current word matches next word in text
+            if (text.getChar(lettersCorrect) == e.getKeyChar()) {
+                lettersTyped = "";
+                lettersCorrect++;
+            } else {
+                lettersTyped += " ";
+                lettersIncorrect++;
+            }
+
+        }
+
+        else if (e.getKeyCode() == KeyEvent.VK_CAPS_LOCK || e.getKeyCode() == KeyEvent.VK_SHIFT){
+        }
+
+        else {
+            System.out.println(e.getKeyChar());
+            lettersTyped += e.getKeyChar();
+            if (e.getKeyChar() == text.getChar(lettersCorrect)) {
+                System.out.println("correct");
+                lettersCorrect++;
+            } else {
+                System.out.println("incorrect");
+                lettersIncorrect++;
+            }
+        }
+
     }
 
 
--- src/TypingGame/TextBlockGenerator.java
+++ src/TypingGame/TextBlockGenerator.java
@@ -16,24 +16,40 @@ public class TextBlockGenerator {
 
     }
 
-    public void draw(Graphics g) {
+    //TODO: change code so that the text block will indicate with color the words correctly/incorrectly typed
+    public void draw(Graphics g, int correct, int incorrect) {
 
         String[] words = text.split("\\s+");
+        String newString = "";
         int splitWidth = 8;
         int prev = 0;
         int yCoord = 100;
-        g.setColor(Color.black);
 
         for (int i = splitWidth; i < words.length; i += splitWidth) {
             //words[i] += "\n";
             String line = String.join(" ", Arrays.copyOfRange(words, prev, i));
-            g.drawString(line, 100, yCoord);
+            newString += (line + "\n");
+            //g.drawString(line, 100, yCoord);
             yCoord += 10;
             prev = i;
         }
 
         String line = String.join(" ", Arrays.copyOfRange(words, prev, words.length));
-        g.drawString(line, 100, yCoord);
+        newString += line;
+        //g.drawString(line, 100, yCoord);
+        g.setColor(Color.green);
+        g.drawString(newString.substring(0, correct), 100, yCoord);
+
+        g.setColor(Color.white);
+        g.drawString(newString.substring(correct, text.length() - incorrect), 100 + g.getFontMetrics().charsWidth(text.substring(0, correct).toCharArray(), 0, correct), yCoord);
+
+        g.setColor(Color.red);
+        g.drawString(newString.substring(text.length() - incorrect, text.length()), 100 + g.getFontMetrics().charsWidth(text.substring(0, text.length() - incorrect).toCharArray(), 0, text.length() - incorrect), yCoord);
+    }
+
+    public char getChar(int position) {
+
+        return text.charAt(position);
     }
 
 }
