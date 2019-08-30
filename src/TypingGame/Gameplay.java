e2ffc1c1d2e7c13ecd19d1df1b128fa7af048da7
Zephaniah Ko <zephaniahko@gmail.com>
2019-08-29 19:37:05 -0700

    placed all key functionalities in methods

--- src/TypingGame/Gameplay.java
+++ src/TypingGame/Gameplay.java
@@ -24,8 +24,8 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
     private boolean newGame = true;
     private int level = 0;
 
-    String[] quotes = {"...he's the hero Gotham deserves, but not the one it needs right now. So, we'll hunt him, because he can take it. Because he's not our hero. He's a silent guardian. A watchful protector. A Dark Knight.",
-    "People need dramatic examples to shake them out of apathy, and I can't do that as Bruce Wayne. As a man, I'm flesh and blood; I can be ignored, I can be destroyed. But as a symbol... as a symbol I can be incorruptible. I can be everlasting.",
+    private String[] quotes = {"...he's the hero Gotham deserves, but not the one it needs right now. So, we'll hunt him, because he can take it. Because he's not our hero. He's a silent guardian. A watchful protector. A Dark Knight.",
+            "People need dramatic examples to shake them out of apathy, and I can't do that as Bruce Wayne. As a man, I'm flesh and blood; I can be ignored, I can be destroyed. But as a symbol... as a symbol I can be incorruptible. I can be everlasting.",
             "A hero can be anyone. Even a man doing something as simple and reassuring as putting a coat around a young boy's shoulders to let him know that the world hadn't ended.",
             "Yeah, I mean, just gotta play our game out there and keep it simple. Can't take these guys lightly, gotta play a full 60. Get pucks to the net with bodies in front. Good group o' guys in here gettin' pucks in deep, sacrificing the body. Need to come out with 2 points.",
     };
@@ -37,42 +37,41 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
     private String lettersTyped = "";
     private int delay = 2;
 
-    int secondsElapsed = 0;
-    double millisElapsed = 0;
-    int wpm, cpm, wpm_final, cpm_final, time_final;
+    private int secondsElapsed = 0;
+    private double millisElapsed = 0;
+    private int wpm, cpm, wpm_final, cpm_final, time_final;
 
     private TextBlockGenerator text = new TextBlockGenerator(quotes[0]);
     private Car car = new Car(100, text.getLength());
     private ScreenElements screen = new ScreenElements();
 
-    java.util.Timer secondsTimer = new java.util.Timer();
-    java.util.TimerTask task = new java.util.TimerTask(){
-
+    private java.util.Timer secondsTimer = new java.util.Timer();
+    private java.util.TimerTask task = new java.util.TimerTask() {
         @Override
         public void run() {
             secondsElapsed++;
         }
     };
 
-    java.util.Timer wpmTimer = new java.util.Timer();
-    java.util.TimerTask task_wpm = new java.util.TimerTask(){
+    private java.util.Timer wpmTimer = new java.util.Timer();
+    private java.util.TimerTask task_wpm = new java.util.TimerTask() {
 
         @Override
         public void run() {
             millisElapsed++;
-            cpm = (int) (((double) lettersCorrect / millisElapsed)* 60000);
-            wpm = (int) (((double) lettersCorrect / millisElapsed)* 60000 / 5);
+            cpm = (int) (((double) lettersCorrect / millisElapsed) * 60000);
+            wpm = (int) (((double) lettersCorrect / millisElapsed) * 60000 / 5);
         }
     };
 
 
-    public Gameplay(){
+    public Gameplay() {
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
         timer = new javax.swing.Timer(delay, this);
         timer.start();
-        System.out.println("gameplay initialized");
+        System.out.println("Gameplay initialized");
 
     }
 
@@ -82,12 +81,11 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 
         if (play) {
 
-            if (gameOver){
+            if (gameOver) {
                 screen.gameOver(g, time_final, wpm_final, cpm_final);
 
-
             } else {
-                text.draw(g, lettersCorrect,lettersIncorrect);
+                text.draw(g, lettersCorrect, lettersIncorrect);
                 screen.printStats(g, secondsElapsed, wpm, cpm);
                 screen.printTypedLetters(g, lettersTyped);
                 car.moveCar(g, lettersCorrect);
@@ -101,7 +99,6 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
     }
 
 
-
     @Override
     public void actionPerformed(ActionEvent e) {
         timer.start();
@@ -110,78 +107,17 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        //TODO: create end sequence when all characters typed
 
-        System.out.println("BEFORE\n " + "letters correct: " + lettersCorrect + "\nletters incorrect: " + lettersIncorrect);
-
-        if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE){
-        //2 branches here: one if correct letter backspaced, one if incorrect letter backspaced
-            if (lettersTyped.length() == 0) {
-            } else {
-
-                if (lettersTyped.charAt(lettersTyped.length() - 1) == ' ') {
-                    lettersIncorrect--;
-                } else if(lettersIncorrect != 0){
-                    lettersIncorrect--;
-                } else if (lettersTyped.charAt(lettersTyped.length() - 1) == text.getChar(lettersCorrect + lettersIncorrect - 1)) {
-                    System.out.println("correct char backspaced");
-                    lettersCorrect--;
-                } else {
-                    lettersIncorrect--;
-                }
-                System.out.println("backspace hit");
-                lettersTyped = lettersTyped.substring(0, lettersTyped.length() - 1);
-                System.out.println(lettersTyped);
-            }
-        }
-
-        //TODO: Add start countdown sequence while showing textblock
-        else if (e.getKeyCode() == KeyEvent.VK_ENTER){
-            //two branches, one if first starting, second if starting new game
-
-            //next game
-            if (gameOver) {
-                gameOver = false;
-                secondsElapsed = lettersIncorrect = lettersCorrect = 0;
-                millisElapsed = 0;
-                level++;
-                lettersTyped = "";
-                text = new TextBlockGenerator(quotes[level]);
-                car = new Car(100, text.getLength());
-
-            }
-            play = true;
-            newGame = true;
-            secondsTimer.scheduleAtFixedRate(task, 0, 1000);
-            wpmTimer.scheduleAtFixedRate(task_wpm, 0, 1);
-        }
-
-        else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-            System.out.println("spacebar hit");
-
-            if (text.getChar(lettersCorrect) == e.getKeyChar() && lettersIncorrect == 0) {
-                lettersTyped = ""; // this resets length to zero so if space is incorrect later on then it wont be able to be backspaced
-                lettersCorrect++;
-            } else {
-                lettersTyped += " ";
-                lettersIncorrect++;
-            }
-        }
-
-        else if (e.getKeyCode() == KeyEvent.VK_CAPS_LOCK || e.getKeyCode() == KeyEvent.VK_SHIFT){
+        if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+            backSpace();
+        } else if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            enter();
+        } else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            space();
+        } else if (e.getKeyCode() == KeyEvent.VK_CAPS_LOCK || e.getKeyCode() == KeyEvent.VK_SHIFT) {
             //no input, skip
-        }
-
-        else {
-            System.out.println(e.getKeyChar());
-            lettersTyped += e.getKeyChar();
-            if (e.getKeyChar() == text.getChar(lettersCorrect) && lettersIncorrect == 0) {
-                System.out.println("correct");
-                lettersCorrect++;
-            } else {
-                System.out.println("incorrect");
-                lettersIncorrect++;
-            }
+        } else {
+            checkChar(e);
         }
 
         //check for game over
@@ -199,7 +135,65 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
     @Override
     public void keyTyped(KeyEvent e) {
     }
+
     @Override
     public void keyReleased(KeyEvent e) {
+    }
+
+    private void backSpace() {
+        if (lettersTyped.length() == 0) {
+        } else {
+            if (lettersTyped.charAt(lettersTyped.length() - 1) == ' ') {
+                lettersIncorrect--;
+            } else if (lettersIncorrect != 0) {
+                lettersIncorrect--;
+            } else if (lettersTyped.charAt(lettersTyped.length() - 1) == text.getChar(lettersCorrect + lettersIncorrect - 1)) {
+                lettersCorrect--;
+            } else {
+                lettersIncorrect--;
+            }
+            lettersTyped = lettersTyped.substring(0, lettersTyped.length() - 1);
+        }
+    }
+
+    private void enter() {
+
+        if (gameOver) {
+            gameOver = false;
+            secondsTimer.cancel();
+            wpmTimer.cancel();
+            secondsElapsed = lettersIncorrect = lettersCorrect = 0;
+            millisElapsed = 0;
+            level++;
+            lettersTyped = "";
+            text = new TextBlockGenerator(quotes[level]);
+            car = new Car(100, text.getLength());
+
+        }
+        play = true;
+        newGame = true;
+        secondsTimer.scheduleAtFixedRate(task, 0, 1000);
+        wpmTimer.scheduleAtFixedRate(task_wpm, 0, 1);
+    }
+
+    private void space() {
+        if (text.getChar(lettersCorrect) == ' ' && lettersIncorrect == 0) {
+            lettersTyped = ""; // this resets length to zero so if space is incorrect later on then it wont be able to be backspaced
+            lettersCorrect++;
+        } else {
+            lettersTyped += " ";
+            lettersIncorrect++;
+        }
+    }
+
+    private void checkChar(KeyEvent e) {
+        lettersTyped += e.getKeyChar();
+        if (e.getKeyChar() == text.getChar(lettersCorrect) && lettersIncorrect == 0) {
+            System.out.println("correct");
+            lettersCorrect++;
+        } else {
+            System.out.println("incorrect");
+            lettersIncorrect++;
+        }
     }
 }
